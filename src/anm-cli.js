9327dde5cb19b7dd20b0c812c19e1817f6e3986f
Eric "dantarion" Sheppard <dantarion@dantarion.com>
2017-05-08 17:02:26 -0700

    Began work on proper CLI using commander (https://github.com/tj/commander.js/)

--- src/anm-cli.js
+++ src/anm-cli.js
@@ -1,17 +1,33 @@
 const fs = require('fs')
 const path = require('path')
-
 const anm = require('./anm')
 
-fs.readdirSync(path.join(process.cwd(), 'out', 'chr')).forEach((filename) => {
-  if (filename.startsWith('.')) {
-    return
-  }
-  console.log(filename)
-  var filePath = path.join(process.cwd(), 'out', 'chr', filename, 'anmchr.anm')
-  try {
-    anm.unpackAnmFile(filePath, filename)
-  } catch (e) {
-    console.log('error', e)
-  }
+const program = require('commander')
+const version = require(path.join(__dirname, '../package.json')).version
+
+program.command('extract <file> <foldername>').description('Extract JS source from .anm file').action((file, foldername) => {
+  console.log(`Extracting ${file} into working/${foldername}`)
+  anm.unpackAnmFile(file, foldername)
 })
+
+program.command('compile <foldername> <file>').description('Compile folder into .anm file').action((file, foldername) => {
+  console.log(`Compiling working/${foldername} into ${file}`)
+  anm.packAnm(file, foldername)
+})
+
+program.command('test').description('Test Suite. Extracts and compiles Ryu\'s file to test rebuilding .anm').action(() => {
+  console.log('Running test suite')
+  fs.readdirSync(path.join(process.cwd(), 'out', 'chr')).forEach((filename) => {
+    if (filename.startsWith('.')) {
+      return
+    }
+    console.log(filename)
+    var filePath = path.join(process.cwd(), 'out', 'chr', filename, 'anmchr.anm')
+    try {
+      anm.unpackAnmFile(filePath, filename)
+    } catch (e) {
+      console.log('error', e)
+    }
+  })
+})
+program.version(version).parse(process.argv)
